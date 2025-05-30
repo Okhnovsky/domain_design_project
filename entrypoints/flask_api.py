1890a6433594222abb3d3f458487a5afa2c9fe19
Александр <alexander-okhnovsky@mail.ru>
2025-05-30 17:21:19 +0300

    add new

--- entrypoints/flask_api.py
+++ entrypoints/flask_api.py
@@ -1,30 +1,37 @@
-from flask import Flask, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+from flask import Flask, jsonify, request
 
-import config
-import model
-import orm
-import repository
-import services
+from allocation.domain import model
+from allocation.adapters import orm
+from allocation.service_layer import services, unit_of_work
 
 
-orm.start_mappers()
-get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
 app = Flask(__name__)
+orm.start_mappers()
 
 
-@app.route("/allocate", methods=["POST"])
-def allocate_endpoint():
-    session = get_session()
-    repo = repository.SqlAlchemyRepository(session)
-    line = model.OrderLine(
-        request.json["orderid"], request.json["sku"], request.json["qty"],
+@app.route("/add_batch", methods=['POST'])
+def add_batch():
+    eta = request.json['eta']
+    if eta is not None:
+        eta = datetime.fromisoformat(eta).date()
+    services.add_batch(
+        request.json['ref'], request.json['sku'], request.json['qty'], eta,
+        unit_of_work.SqlAlchemyUnitOfWork(),
     )
+    return 'OK', 201
 
+
+@app.route("/allocate", methods=['POST'])
+def allocate_endpoint():
     try:
-        batchref = services.allocate(line, repo, session)
+        batchref = services.allocate(
+            request.json['orderid'],
+            request.json['sku'],
+            request.json['qty'],
+            unit_of_work.SqlAlchemyUnitOfWork(),
+        )
     except (model.OutOfStock, services.InvalidSku) as e:
-        return {"message": str(e)}, 400
+        return jsonify({'message': str(e)}), 400
 
-    return {"batchref": batchref}, 201
+    return jsonify({'batchref': batchref}), 201
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,7 +1,6 @@
 import pytest
-import model
-import repository
-import services
+from allocation.adapters import repository
+from allocation.service_layer import services, unit_of_work
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -19,37 +18,39 @@ class FakeRepository(repository.AbstractRepository):
         return list(self._batches)
 
 
-class FakeSession:
-    committed = False
-
-    def commit(self):
-        self.committed = True
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
+    ...
 
 
-def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
-
-    result = services.allocate(line, repo, FakeSession())
-    assert result == "b1"
-
-
-def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
-    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
-    repo = FakeRepository([batch])
-
-    with pytest.raises(services.InvalidSku,
-                       match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line, repo, FakeSession())
+def test_add_batch():
+    uow = FakeUnitOfWork()
+    # fake_uow_starter = FakeUoWContextManager(uow) ?
+    # fake_uow_starter = contextlib.nullcontext(uow) ?
+    # services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, fake_uow_starter)
+    assert uow.batches.get("b1") is not None
+    assert uow.committed
 
 
-def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
-    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    repo = FakeRepository([batch])
-    session = FakeSession()
+@pytest.mark.skip('unskip and fix when ready')
+def test_allocate_returns_allocation():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+    assert result == "batch1"
 
-    services.allocate(line, repo, session)
-    assert session.committed is True
+
+@pytest.mark.skip('unskip and fix when ready')
+def test_allocate_errors_for_invalid_sku():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "AREALSKU", 100, None, uow)
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
+
+
+@pytest.mark.skip('unskip and fix when ready')
+def test_allocate_commits():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    assert uow.committed
